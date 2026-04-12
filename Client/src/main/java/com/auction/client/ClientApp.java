6a4eecc0c78124519a3cc332b70e45421ace3eeb
peony063120 <suyin9520@gmail.com>
2026-04-12 21:36:56 +0700

    change

---
+++ Client/src/main/java/com/auction/client/ClientApp.java
@@ -0,0 +1,62 @@
+package com.auction.client;
+
+import com.auction.client.network.ServerConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ClientApp extends Application {
+    private static Stage primaryStage;
+    private static ServerConnection serverConnection;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+
+        // Initialize server connection
+        serverConnection = new ServerConnection("localhost", 9999);
+        serverConnection.connect();
+
+        // Load login screen
+        showLoginScreen();
+
+        stage.setTitle("Online Auction System");
+        stage.setMinWidth(1024);
+        stage.setMinHeight(768);
+        stage.show();
+    }
+
+    public static void showLoginScreen() throws Exception {
+        FXMLLoader loader = new FXMLLoader(ClientApp.class.getResource("/view/login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(ClientApp.class.getResource("/styles/main.css").toExternalForm());
+        primaryStage.setScene(scene);
+    }
+
+    public static void showMainScreen() throws Exception {
+        FXMLLoader loader = new FXMLLoader(ClientApp.class.getResource("/view/main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(ClientApp.class.getResource("/styles/main.css").toExternalForm());
+        primaryStage.setScene(scene);
+    }
+
+    public static ServerConnection getServerConnection() {
+        return serverConnection;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop() {
+        if (serverConnection != null) {
+            serverConnection.disconnect();
+        }
+    }
+}
